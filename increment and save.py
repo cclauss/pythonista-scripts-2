3141eb04e2c16a7d4a8353707887c97eb4a2659d
cclauss <cclauss@bluewin.ch>
2015-07-08 22:05:43 +0200

    Simplify logic with num = int(num) if num else 0

--- increment and save.py
+++ increment and save.py
@@ -9,18 +9,14 @@ if not text:
 
 filename = os.path.split(editor.get_path())[1][:-3]
 
-#finds number at end
+#find number at end of filename
 num = re.split('[^\d]', filename)[-1]
 
-#check if number string is empty
-#if file ends with a number, increment it
-l = len(num)
+#converts num from string to integer
+num = int(num) if num else 0
 
-if l >=1:
-	num2 = int(num) + 1
-	filename = filename[:-l] + str(num2) + ".py"
-else:
-	filename = filename + str(1) + ".py"
+#rebuild filename with incremented number
+filename = '{}{}.py'.format(filename[:-len(num)] + num + 1)
 
 #write new file
 editor.make_new_file(filename, text)
